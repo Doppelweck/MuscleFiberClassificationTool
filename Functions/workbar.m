b93fa837cceecaaf2d9c6f6464b399120039087a
Sebastian Friedrich <sebastian.friedrich89@gmail.com>
2023-12-27 12:10:35 +0100

    add return value to workbar

--- Functions/workbar.m
+++ Functions/workbar.m
@@ -1,4 +1,4 @@
-function workbar(fractiondone, message, progtitle, mainFig,isVisible)
+function percentdone=workbar(fractiondone, message, progtitle, mainFig,isVisible)
 % WORKBAR Graphically monitors progress of calculations
 %   WORKBAR(X) creates and displays the workbar with the fractional length
 %   "X". It is an alternative to the built-in matlab function WAITBAR,
