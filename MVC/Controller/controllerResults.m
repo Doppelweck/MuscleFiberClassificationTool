ae1b2b8741361468a03ab02e0b8a008e2307d82b
Sebastian Friedrich <sebastian.friedrich89@gmail.com>
2023-12-29 12:52:34 +0100

    update errorMessage() in controllers

--- MVC/Controller/controllerResults.m
+++ MVC/Controller/controllerResults.m
@@ -1721,18 +1721,17 @@ classdef controllerResults < handle
         end
         
         function errorMessage(obj,ErrorInfo)
-            Text = [];
+            Text = cell(5*size(ErrorInfo.stack,1)+2,1);
             Text{1,1} = ErrorInfo.message;
             Text{2,1} = '';
             
             if any(strcmp('stack',fieldnames(ErrorInfo)))
                 for i=1:size(ErrorInfo.stack,1)
-                    
-                    Text{end+1,1} = [ErrorInfo.stack(i).file];
-                    Text{end+1,1} = [ErrorInfo.stack(i).name];
-                    Text{end+1,1} = ['Line: ' num2str(ErrorInfo.stack(i).line)];
-                    Text{end+1,1} = '------------------------------------------';
-                    
+                    idx = (i - 1) * 5 + 2;
+                    Text{idx+1,1} = [ErrorInfo.stack(i).file];
+                    Text{idx+2,1} = [ErrorInfo.stack(i).name];
+                    Text{idx+3,1} = ['Line: ' num2str(ErrorInfo.stack(i).line)];
+                    Text{idx+4,1} = '------------------------------------------';  
                 end
             end
             
