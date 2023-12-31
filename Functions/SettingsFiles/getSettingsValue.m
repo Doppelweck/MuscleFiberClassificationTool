57a6c240ba26416cc9186d07924461fe09c3a64f
Sebastian Friedrich <sebastian.friedrich89@gmail.com>
2023-12-31 12:38:27 +0100

    optimized getSettingsValue

--- Functions/SettingsFiles/getSettingsValue.m
+++ Functions/SettingsFiles/getSettingsValue.m
@@ -1,13 +1,19 @@
 function value = getSettingsValue(searchString)
-
 currentFile = mfilename( 'fullpath' );
+
 [pathstr,~,~] = fileparts( currentFile );
-addpath( pathstr );
+
+if(~contains(matlabpath, pathstr))
+    addpath( pathstr );
+end
+
 data=load(fullfile( pathstr, 'AppSettings.mat' ));
 
 idx=find(contains(data.Settings,searchString));
 
 if(~isempty(idx))
     value = data.Settings{idx,2};
+else
+    value = [];
 end
 end
