37f39b5caf46d70ebccb3ba672b64ab7293a6a4f
Sebastian Friedrich <sebastian.friedrich89@gmail.com>
2024-01-18 16:44:02 +0100

    update chakAppForNewVersion

--- Functions/SettingsFiles/checkAppForNewVersion.m
+++ Functions/SettingsFiles/checkAppForNewVersion.m
@@ -1,7 +1,8 @@
-function [newVersionAvailable, checkSuccessfull] = checkAppForNewVersion()
+function [newVersionAvailable, checkSuccessfull, newVersion] = checkAppForNewVersion(versionString)
 
     temp_newVersionAvailable = false;
     temp_checkSuccessfull = false;
+    temp_newVersion = '';
     
     fileContent = '';
 
@@ -13,8 +14,47 @@ function [newVersionAvailable, checkSuccessfull] = checkAppForNewVersion()
         temp_checkSuccessfull = false;
     end
     
+    if(temp_checkSuccessfull)
+        % Define a regular expression pattern
+        pattern = '(\d+(\.\d+)*)';
+
+        % Use regular expression to find a match
+        matchfileContent = regexp(fileContent, pattern, 'match', 'once');
+        if ~isempty(matchfileContent)
+          gitMasterVersion = str2double(strsplit(['.' matchfileContent], '.'));
+        end
+        matchversionString = regexp(versionString, pattern, 'match', 'once');
+        if ~isempty(matchversionString)
+            appVersion = str2double(strsplit(['.' matchversionString], '.'));
+        end
+        
+        % Ensure both version arrays have the same length
+        gitMasterVersion(1) = [];
+        appVersion(1) = [];
+        maxLength = max(length(gitMasterVersion), length(appVersion));
+        gitMasterVersion(end+1:maxLength) = 0;
+        appVersion(end+1:maxLength) = 0;
+        
+        
+        for i = 1:min(length(gitMasterVersion), length(appVersion))
+            if gitMasterVersion(i) > appVersion(i)
+                temp_newVersionAvailable = true;
+                temp_newVersion = matchfileContent;
+                break;  % Exit the loop
+            end
+        end
+
+        % Compare version numbers
+        if all(gitMasterVersion > appVersion)
+            disp('The extracted version is greater than or equal to the reference version.');
+        else
+            disp('The extracted version is less than the reference version.');
+        end
+    end
+    
     
     %RETURN
     newVersionAvailable = temp_newVersionAvailable;
     checkSuccessfull = temp_checkSuccessfull;
+    newVersion = temp_newVersion;
 end
