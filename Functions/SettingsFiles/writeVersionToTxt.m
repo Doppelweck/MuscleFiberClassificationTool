62f6deb2d2f545443756fe1e96a00007b086d167
Sebastian Friedrich <sebastian.friedrich89@gmail.com>
2024-01-19 09:15:51 +0100

    LATEST.txt now contains update information's

--- Functions/SettingsFiles/writeVersionToTxt.m
+++ Functions/SettingsFiles/writeVersionToTxt.m
@@ -1,17 +1,37 @@
 function writeVersionToTxt(versionString)
+inputFilePath = 'LATEST.txt';
+outputFilePath = 'LATEST.txt';
 
 % Open a text file for writing
-fileID = fopen('LATEST.txt', 'w');
+fid = fopen(inputFilePath, 'r');
 
 % Check if the file was opened successfully
-if fileID == -1
-    error('Could not open the file for writing.');
+if fid == -1
+    error('ERROR writeVersionToTxt(): Could not open the file for writing.');
+else
+   
+    
+% New string to replace the first line
+newFirstLine = versionString;
+
+% Read the entire content of the file
+fileContent = fread(fid, '*char')';  % Read entire content as a string
+fclose(fid);
+
+% Split the content into lines
+lines = strsplit(fileContent, '\n', 'CollapseDelimiters', false);
+
+% Replace the first line
+lines{1} = newFirstLine;
+
+% Join the lines back into a single string
+modifiedContent = strjoin(lines, '\n');
+
+% Write the modified content back to the file
+fid = fopen(outputFilePath, 'w');
+fprintf(fid, '%s', modifiedContent);
+fclose(fid);
+    
 end
-
-% Write the text to the file
-fprintf(fileID, '%s\n', versionString);
-
-% Close the file
-fclose(fileID);
 
 end
